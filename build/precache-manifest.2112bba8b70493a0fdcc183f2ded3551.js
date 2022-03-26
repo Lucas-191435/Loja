b6957efeae8d52adaac3de896f79b16b0f22e00c
Lucas Fernando <lucasfernandosantos2001@gmail.com>
2022-03-25 21:02:12 -0300

    main

---
+++ build/precache-manifest.2112bba8b70493a0fdcc183f2ded3551.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bea47f3dfe2087850d290d024ca348",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad55619cd46d272a088",
+    "url": "/static/css/4.5cc0b209.chunk.css"
+  },
+  {
+    "revision": "53cefb963f45b764106e",
+    "url": "/static/css/main.3cb10e5f.chunk.css"
+  },
+  {
+    "revision": "bef21e05f68d8dd19ec2",
+    "url": "/static/js/2.fe60626c.chunk.js"
+  },
+  {
+    "revision": "0fc0d22dc8fb6c3dc2e4",
+    "url": "/static/js/3.640ecc8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/3.640ecc8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ad55619cd46d272a088",
+    "url": "/static/js/4.8cbb337a.chunk.js"
+  },
+  {
+    "revision": "53cefb963f45b764106e",
+    "url": "/static/js/main.8c9c5e30.chunk.js"
+  },
+  {
+    "revision": "f4133c59df84a1a4d517",
+    "url": "/static/js/runtime-main.f813a531.js"
+  },
+  {
+    "revision": "1493e279f0795e41d51b73d63452c20f",
+    "url": "/static/media/Loja.1493e279.svg"
+  },
+  {
+    "revision": "4a6d6ceb26b2d64c10a2b26fa9c47aaf",
+    "url": "/static/media/adicionar.4a6d6ceb.svg"
+  },
+  {
+    "revision": "9f80ad3de4a46815a44aa1a78bfc37dd",
+    "url": "/static/media/enviar.9f80ad3d.svg"
+  },
+  {
+    "revision": "5b48807f51a9c4c87f8a5f802cf35aa0",
+    "url": "/static/media/estatisticas.5b48807f.svg"
+  },
+  {
+    "revision": "35f798d01b74896299f27c50c3cd6816",
+    "url": "/static/media/feed.35f798d0.svg"
+  },
+  {
+    "revision": "4175ea7207f56280b48714a4133289cb",
+    "url": "/static/media/sair.4175ea72.svg"
+  },
+  {
+    "revision": "dc4429925d170a392d67275b0e5f43b2",
+    "url": "/static/media/usuario.dc442992.svg"
+  },
+  {
+    "revision": "957befc3d57033c55a88d144ae173685",
+    "url": "/static/media/visualizacao-black.957befc3.svg"
+  },
+  {
+    "revision": "4ce15ed8e41974f50628587af3b32c4b",
+    "url": "/static/media/visualizacao.4ce15ed8.svg"
+  }
+]);
